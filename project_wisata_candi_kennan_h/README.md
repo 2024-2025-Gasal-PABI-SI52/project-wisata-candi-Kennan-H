7173239e60369d7334e39ef50bec369c7edc8371
Kennan-H <kiriyoramita@gmail.com>
2024-11-09 18:20:28 +0700

    Push 1

---
+++ project_wisata_candi_kennan_h/README.md
@@ -0,0 +1,3 @@
+# project_wisata_candi_kennan_h
+
+A new Flutter project.
---
+++ project_wisata_candi_kennan_h/android/app/src/main/kotlin/com/example/project_wisata_candi_kennan_h/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.project_wisata_candi_kennan_h
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
